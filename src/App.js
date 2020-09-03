a69178d0f1aa28025dacb4d6fe76988c903af2f9
loc-koan <loc.koan@yahoo.com>
2020-09-03 18:58:45 -0500

    added fontawesome, started sort method

--- src/App.js
+++ src/App.js
@@ -5,12 +5,35 @@ import Title from "./components/Title";
 import SearchForm from "./components/SearchForm";
 // import Table from "./components/Table";
 import friends from "./friends.json";
+// import { fontAwesomeIcon } from './components/FontAwesomeIcons'
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    order: "desc",
+    sortFriends: [{}]
   };
+
+  // headings = [
+  //   {name: "Name" }
+  // ]
+
+  // handleSort = head => {
+  //   console.log("Inside Handle Sort");
+
+  //   if(this.state.order === "desc"){
+  //     this.setState({
+  //       order: "asc"
+  //     })
+  //   }
+  //   else{
+  //     this.setState({
+  //       order: "desc"
+  //     })
+  //   }
+  //   }
+  // }
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
@@ -32,7 +55,8 @@ class App extends Component {
         <table className="container">
           <thead className="row">
             <th className="col-md-2 col-md-offset-1">Image</th>
-            <th className="col-md-2">Name</th>
+            <th className="col-md-2">Name <button id="sort-asc" onClick={this.handleSort}><i class="fas fa-angle-down"></i></button></th>
+            {/* <fontAwesomeIcon icon="fa-caret-up"> */}
             <th className="col-md-2">Phone</th>
             <th className="col-md-3">Email</th>
             <th className="col-md-2">DOB</th>
---
+++ src/components/FontAwesomeIcons/index.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/react-fontawesome';
+import { fa-caret-up } from '@fortawesome/react-fontawesome';
+
+library.add(fa-caret-up);
