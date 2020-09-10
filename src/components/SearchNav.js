78bbdb4e6fb8724e36e14b7a12354b0d6f3a1aaa
loc-koan <loc.koan@yahoo.com>
2020-09-09 23:47:59 -0500

    added SearchNav and TableInfo js files

---
+++ src/components/SearchNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchForm from "./SearchForm.js";
+import "../styles/Nav.css";
+
+function SearchNav({ handleSearchChange }) {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <SearchForm handleSearchChange={handleSearchChange} />
+      </div>
+    </nav>
+  );
+}
+export default SearchNav;
