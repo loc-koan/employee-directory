68923b646b80648aea8988eea785bfbcb68c7619
loc-koan <loc.koan@yahoo.com>
2020-09-12 13:55:50 -0500

    edited gitignore

---
+++ build/precache-manifest.49614e21221ce32539d58e9f75cda3d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0d697de24fe4a3f939f5956813f713",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "a6212a4eb4d067fba155",
+    "url": "/employee-directory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "01f32d64a2833e53201b",
+    "url": "/employee-directory/static/css/main.02cbd020.chunk.css"
+  },
+  {
+    "revision": "a6212a4eb4d067fba155",
+    "url": "/employee-directory/static/js/2.5b84fd3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.5b84fd3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f32d64a2833e53201b",
+    "url": "/employee-directory/static/js/main.a064ce68.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
